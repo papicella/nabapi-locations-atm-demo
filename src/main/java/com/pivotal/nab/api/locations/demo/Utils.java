556d2c1346356f3ada4a53983dca0fdcdc514efe
papicella <pas.apicella@tpg.com.au>
2017-02-01 22:14:16 +1100

    Initial Commit

--- src/main/java/com/pivotal/nab/api/locations/demo/Utils.java
+++ src/main/java/com/pivotal/nab/api/locations/demo/Utils.java
@@ -9,15 +9,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 public class Utils
 {
     private static Log logger = LogFactory.getLog(Utils.class);
     private static final JsonParser parser = JsonParserFactory.getJsonParser();
 
-    public static String getLocations (String nabApiKey) throws Exception
+    public static String getLocations(String nabApiKey,
+                                      String swLat,
+                                      String neLat,
+                                      String neLng,
+                                      String swLng) throws Exception
     {
         RestTemplate restTemplate = new RestTemplate();
-        String uri = "TODO://";
+        String uri = "https://api.developer.nab.com.au/v2/locations?locationType=atm&searchCriteria=geo&swLat=%s&swLng=%s&neLat=%s&neLng=%s&fields=extended&v=1";
+
+        logger.info(String.format(uri, swLat, swLng, neLat, neLng));
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("x-nab-key", nabApiKey);
@@ -25,15 +33,12 @@ public class Utils
         HttpEntity<String> entity = new HttpEntity<String>(headers);
         HttpEntity<String> response = null;
 
-        response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+        response = restTemplate.exchange(String.format(uri, swLat, swLng, neLat, neLng), HttpMethod.GET, entity, String.class);
 
-        /*
         Map<String, Object> jsonMap = parser.parseMap(response.getBody());
-        Map fxRatesMap = (Map) jsonMap.get("fxRatesResponse");
-        List<FxRate> ratesList = (List<FxRate>) fxRatesMap.get("fxRates");
-        */
+        logger.info(jsonMap);
 
-        return "";
+        return response.getBody();
     }
 
 }
--- src/main/java/com/pivotal/nab/api/locations/demo/controller/LocationsController.java
+++ src/main/java/com/pivotal/nab/api/locations/demo/controller/LocationsController.java
@@ -1,5 +1,6 @@
 package com.pivotal.nab.api.locations.demo.controller;
 
+import com.pivotal.nab.api.locations.demo.Utils;
 import com.pivotal.nab.api.locations.demo.beans.NabApiKey;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LocationsController
@@ -22,6 +24,24 @@ public class LocationsController
     {
         logger.info("Invoking LocationsController");
         logger.info(nabApiKey.toString());
+
+        return "locationsapi";
+    }
+
+    @RequestMapping(value="/search", method = RequestMethod.POST)
+    public String searchAtms
+            (@RequestParam(value="swLat") String swLat,
+             @RequestParam(value="neLat") String neLat,
+             @RequestParam(value="neLng") String neLng,
+             @RequestParam(value="swLng") String swLng,
+             Model model) throws Exception
+    {
+        logger.info("Invoking LocationsController POST search action");
+        logger.info(nabApiKey.toString());
+
+        String response = Utils.getLocations(nabApiKey.toString(), swLat, neLat, neLng, swLng);
+
+        logger.info(response);
 
         return "locationsapi";
     }
